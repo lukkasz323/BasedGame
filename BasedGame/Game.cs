53d6052d41e061722bbe473baa6ab5cf516c096f
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-05-19 06:00:07 +0200

    Add basic game over condition

--- BasedGame/Game.cs
+++ BasedGame/Game.cs
@@ -2,13 +2,36 @@
 
 internal class Game
 {
+    internal bool ShouldContinue { get; set; } = true;
     internal GameState State { get; } = new();
 
     internal void Run()
     {
-        while (true)
+        Draw();
+        while (ShouldContinue)
         {
-            Console.WriteLine(State.Player.Health);
+            ShouldContinue = Update();
+            Draw();
         }
+    }
+
+    private bool Update()
+    {
+        bool shouldGameContinue = true;
+
+        ConsoleKeyInfo input = Console.ReadKey(true);
+
+        State.Player.Health -= 1;
+        if (State.Player.Health <= 0)
+        {
+            shouldGameContinue = false;
+        }
+
+        return shouldGameContinue;
+    }
+
+    private void Draw()
+    {
+        Console.WriteLine(State.Player.Health);
     }
 }
--- BasedGame/Player.cs
+++ BasedGame/Player.cs
@@ -2,5 +2,15 @@
 
 internal class Player : Entity
 {
-    internal int Health { get; set; } = 100;
+    private int _maxHealth = 100;
+    private int _health;
+
+    internal Player()
+    {
+        _health = _maxHealth;
+    }
+
+    internal int Health { get => _health; set => _health = Math.Clamp(value, 0, _maxHealth); }
+    internal int Weapon { get; set; }
+    internal int Armor { get; set; }
 }
