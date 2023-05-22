38b67b185c02f0df23aca8a871368f2047141cc1
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-05-22 10:46:12 +0200

    Add level ups, (+)

--- BasedGame/Entities/Player.cs
+++ BasedGame/Entities/Player.cs
@@ -4,13 +4,35 @@ namespace BasedGame.Entities;
 
 internal class Player : Entity
 {
-    internal int Level { get; set; }
-    internal int Xp { get; set; }
-    internal int MaxXp { get; set; }
+    private int _xp = 0;
+
+    internal int Level { get; private set; } = 1;
+    internal int MaxXp { get; private set; } = 10;
     internal CombatComponent Combat { get; } = new();
+
+    internal int Xp 
+    {
+        get => _xp;
+        set 
+        {
+            _xp = value;
+            if (_xp >= MaxXp)
+            {
+                LevelUp();
+            }
+        }
+    }
 
     internal Player()
     {
+        Combat.MaxHealth = 1000;
+        Combat.Health = 1000;
         Combat.Strength = 2;
+    }
+
+    private void LevelUp()
+    {
+        _xp -= MaxXp;
+        MaxXp *= 2;
     }
 }
