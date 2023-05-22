ba40f4163f7a8f366610ec6766666bd450b6ede5
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-05-22 07:17:39 +0200

    Add base component

---
+++ BasedGame/Components/Component.cs
@@ -0,0 +1,13 @@
+﻿using BasedGame.Entities;
+
+namespace BasedGame.Components;
+
+internal class Component
+{
+    internal Entity Parent { get; }
+
+    internal Component(Entity parent)
+    {
+        Parent = parent;
+    }
+}
